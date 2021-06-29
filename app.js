7b907128e8a58180d949a85661b5dbf2820bd074
Jose Cisneros <josecis0216@gmail.com>
2021-06-28 18:05:37 -0600

    initial commit

--- app.js
+++ app.js
@@ -2,23 +2,116 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://JoseC:GoSpFW1end9VvK9O@cluster0.4hqym.mongodb.net/shop?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrffProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination:(req, file, cb) => {
+    cb(null, 'images');
+  } ,
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false); 
+  }
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({storage: fileStorage }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrffProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if(!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken()
+  next();
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+})
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    // User.findOne().then(user => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: 'Jose',
+    //       email: 'jose@test.com',
+    //       cart: {
+    //         items: []
+    //       }
+    //     });
+    //     user.save();
+    //   }
+    // });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -10,10 +11,16 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/orders', shopController.getOrders);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.get('/checkout', shopController.getCheckout);
+router.post('/cart', isAuth, shopController.postCart);
+
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+
+router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
